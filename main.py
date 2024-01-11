a655e6d43b354e2d0757a373f6c05e7a1c306f2d
Lokman <ramdani.lokman@gmail.com>
2024-01-11 15:40:28 +0100

    v2

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ app.get("/transactions-in-high-income-cities")(transactions_in_high_income_citie
 
 # Main function to run the app
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
